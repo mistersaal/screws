4e1835991baaa83424a6576c3fe48072ce91bef9
Степан Сааль <mister.saal@yandex.ru>
2020-01-24 19:37:46 +0300

    Убрал стандартный фронт Laravel, подключил Bulma и Fontawesome. Создал некий шаблон сайта.

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <h5 class="title is-6">Контакты:</h5>
+                <div class="buttons are-small">
+                    <a href="#" class="button is-link is-selected">
+                        <span class="icon"><i class="fab fa-vk"></i></span>
+                        <span>Vk</span>
+                    </a>
+                    <a href="#" class="button is-link">
+                        <span class="icon"><i class="fab fa-instagram"></i></span>
+                        <span>Inst</span>
+                    </a>
+                    <a href="#" class="button is-link">
+                        <span class="icon"><i class="fab fa-facebook"></i></span>
+                        <span>Facebook</span>
+                    </a>
+                </div>
+            </div>
+            <div class="column">
+                <h5 class="title is-6">Адрес:</h5>
+                <p>Блаблабла</p>
+            </div>
+            <div class="column">
+                <h5 class="title is-6">О нас:</h5>
+                <p>Блаблабла</p>
+            </div>
+            <div class="column">
+                <h5 class="title is-6">Ещё что-то:</h5>
+                <p>Блаблабла</p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar is-fixed-top">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+                <span class="title is-3">Saal</span>
+            </a>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <a href="#" class="navbar-item">
+                    Home
+                </a>
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">More</a>
+                    <div class="navbar-dropdown">
+                        <a href="#" class="navbar-item">Link 1</a>
+                        <a href="#" class="navbar-item">Link 2</a>
+                        <a href="#" class="navbar-item">Link 3</a>
+                        <a href="#" class="navbar-item">Link 4</a>
+                        <a href="#" class="navbar-item">Link 5</a>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="field has-addons">
+                        <div class="control">
+                            <input type="text" class="input" id="search" name="search" placeholder="Search">
+                        </div>
+                        <div class="control">
+                            <button class="button is-info">Go</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
