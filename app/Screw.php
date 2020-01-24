f143939693265bf74dad69e1830ac286803aa533
Степан Сааль <mister.saal@yandex.ru>
2020-01-24 22:09:36 +0300

    Добавил зависимости между саморезами и их параметрами.

--- app/Screw.php
+++ app/Screw.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Screw extends Model
 {
-    //
+
+    public function color()
+    {
+        return $this->hasOne(ScrewColor::class);
+    }
+
+    public function length()
+    {
+        return $this->hasOne(ScrewLength::class);
+    }
+
+    public function manufacturer()
+    {
+        return $this->hasOne(ScrewManufacturer::class);
+    }
+
+    public function type()
+    {
+        return $this->hasOne(ScrewType::class);
+    }
+
 }
--- app/ScrewType.php
+++ app/ScrewType.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ScrewType extends Model
 {
-    //
+    protected $timestamps = false;
 }
